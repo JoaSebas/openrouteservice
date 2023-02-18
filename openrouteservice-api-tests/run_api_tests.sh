9fc0b5d18266a73db7173a8ba2b2c005ba90bec3
Julian Psotta <julian.psotta@heigit.org>
2023-02-18 11:52:16 +0100

    debug: print debug line

--- openrouteservice-api-tests/run_api_tests.sh
+++ openrouteservice-api-tests/run_api_tests.sh
@@ -2,6 +2,12 @@
 while [ ! -f  $1/ors.run ]
 do
   echo "DEBUG: Waiting in folder: $1"
+  echo "DEBUG: The folder is containing:"
+  echo ""
+  ls -sahlS
+  echo "DEBUG: The parent folder contains:"
+  echo""
+  ls -sahlS
   sleep 3
 done
 mvn -B test --file $1/openrouteservice-api-tests/pom.xml
